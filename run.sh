51c44fe0e6679912062cdfee0b774c5e0e144197
心言自由 <XinYaanZyoy@gmail.com>
2020-12-23 01:32:47 +0530

    Delete run.sh

--- run.sh
+++
@@ -1,4 +0,0 @@
-# install dependencies
-pip3 install jupyter pandas matplotlib rise # install jupyter notebook, pandas(and dependant numpy), matplotlib
-jupyter-nbextension install rise --py --sys-prefix
-jupyter nbextension enable rise --py --sys-prefix
